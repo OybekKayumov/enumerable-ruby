76fdf5873bd764765701a3a8d1826cad27e7d0a7
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-09 20:34:17 +0500

    add linters

--- my_list.rb
+++ my_list.rb
@@ -1,15 +1,15 @@
 require_relative 'my_enumerable'
 
 class MyList
-  include MyEnumerable
+  include MyEnumerable
 
-  def initialize(*list)
-    @list = list
-  end
+  def initialize(*list)
+    @list = list
+  end
 
-  def each(&block)
-    @list.each(&block)
-  end
+  def each(&block)
+    @list.each(&block)
+  end
 end
 
 list = MyList.new(1, 2, 3, 4)
@@ -20,5 +20,3 @@ puts(list.all? { |e| e > 5 })
 puts(list.any? { |e| e == 2 })
 
 puts(list.filter(&:even?))
-
-
